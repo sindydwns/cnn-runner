9c7966337d2d5851dac144cd4ca4462135264898
sindydwns <sindydwns@naver.com>
2024-07-24 14:08:56 +0900

    asdf

---
+++ p_efficientnet_b7_v2/define.py
@@ -0,0 +1,27 @@
+from torchvision import transforms
+import torchvision.models as models
+import torchvision
+from torch import nn
+
+def image_norm():
+    mean = (0.4914,0.4882,0.4465)
+    std = (0.247,0.243,0.261)
+    return mean, std
+
+def transform():
+    mean, std = image_norm()
+    return transforms.Compose([
+    transforms.Resize(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=mean, std=std),
+])
+
+def create_model():
+    model = models.efficientnet_b7(weights=torchvision.models.EfficientNet_B7_Weights.DEFAULT)
+    model.classifier = nn.Sequential(
+        nn.Linear(2560, 5),
+    )
+    return model
+
+def model_desc():
+    return "efficientnet_b7 with low lr"
