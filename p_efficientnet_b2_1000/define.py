f1a2368be110154d5fa7e5f43bda3e3b4e4710e5
sindydwns <sindydwns@naver.com>
2024-07-27 15:23:37 +0900

    add efficientnets

---
+++ p_efficientnet_b2_1000/define.py
@@ -0,0 +1,31 @@
+from torchvision import transforms
+import torchvision.models as models
+import torchvision
+from torch import nn
+
+def image_norm():
+    mean = (0.4914,0.4882,0.4465)
+    std = (0.247,0.243,0.261)
+    return mean, std
+
+def transform():
+    mean, std = image_norm()
+    return transforms.Compose([
+        transforms.Resize(224),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean, std=std),
+    ])
+
+def create_model():
+    model = models.efficientnet_b2(weights=torchvision.models.EfficientNet_B2_Weights.DEFAULT)
+    model.classifier = nn.Sequential(
+        nn.Linear(1408, 5),
+    )
+    return model
+
+def model_desc():
+    return "efficientnet_b7 1000"
+
+def get_datafolder():
+    return "./dataset_1000"
